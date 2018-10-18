c55bddd8bde22b97d4f479dc71b9ca7c9c3bac0e
Olivier Rousseau <zeachco@gmail.com>
2018-10-18 16:51:47 -0400

    added kill process

--- bash_custom.sh
+++ bash_custom.sh
@@ -68,3 +68,8 @@ ipp () {
 ipl () {
   ifconfig | grep broadcast | awk '{print $2}'
 }
+
+# Kill all processes that match the given name. ie: `killname webpack` will kill all running webpack instances
+killname() {
+  sudo kill -9 $(ps -e | grep $1 | awk '{print $1}')
+}
